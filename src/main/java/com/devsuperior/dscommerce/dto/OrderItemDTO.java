b17b49150c1caa93de013c1366e9527e8f4b8d88
mariomoreirasilva <mario@desban.org.br>
2024-05-02 10:04:46 -0300

    Acrescentando url em OrderItemDTO

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -9,12 +9,14 @@ public class OrderItemDTO {
 	private Double price;
 	private Integer quantity;
 	private Double subtotal;
+	private String imgUrl;
 	
-	public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {		
+	public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {		
 		this.productId = productId;
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+		this.imgUrl = imgUrl;
 	}
 	
 	public OrderItemDTO(OrderItem entity) {		
@@ -22,6 +24,7 @@ public class OrderItemDTO {
 		name = entity.getProduct().getName();
 		price = entity.getPrice();
 		quantity = entity.getQuantity();
+		imgUrl = entity.getProduct().getImgUrl();
 	}
 
 	public Long getProductId() {
@@ -43,6 +46,10 @@ public class OrderItemDTO {
 	public Double getsubTotal() {
 		return price * quantity;
 				
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
 	}
 	
 	
